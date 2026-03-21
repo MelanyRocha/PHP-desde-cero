adf644a96a6c1becfca07fda2f6d7d4259d1715c
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-21 14:39:56 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -8,4 +8,11 @@ echo $my_String . "\n";
 echo gettype($my_String) . "\n";
 $my_String = "esto es cadena";
 
+$my_int = 7;
+$my_int = $my_int + 4;
+echo $my_int . "\n";
+echo $my_int - 1 . "\n";
+echo $my_int . "\n";
+echo gettype($my_int) . "\n";
+
 ?>
