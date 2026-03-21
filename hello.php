e66b903c8e708259e4f2a34b4431833af2310bfc
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-21 14:16:52 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@ echo "helloo Mel \n";
 
 $my_String = "esto es cadena";
 $my_String = "cabio ahhhh xxd";
-//$my_String = 6; // tipado dinamico
+$my_String = 6; // tipado dinamico
 echo $my_String . "\n";
 echo gettype($my_String) . "\n";
 $my_String = "esto es cadena";
