a7d28fb18de56e99e80b510cfa6560f4ddb4e7ea
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-23 09:57:59 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -21,6 +21,7 @@ $my_double = 6.5;
 echo gettype($my_double) . "\n";
 echo $my_int + $my_double . "\n";
 //echo $my_int + $my_double + $my_String . "\n"; -- esto no se permite
+// prueba de contribucion
 
 $my_bool = true;
 echo $my_bool . "\n";
